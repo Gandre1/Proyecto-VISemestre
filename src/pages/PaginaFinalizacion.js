262ae3a48a6b14354eab605c98dd23a5f696381f
Gandre1 <116277140+Gandre1@users.noreply.github.com>
2023-10-20 19:17:56 -0500

    .

--- src/pages/PaginaFinalizacion.js
+++ src/pages/PaginaFinalizacion.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import FormularioFinalizacion from '../components/FormularioFinalizacion';
-import Navbar from '../components/Navbar';
 
 function PaginaFinalizacion() {
   return (
     <div>
-      <Navbar />
       <center><h1>Finalización</h1></center>
       <FormularioFinalizacion />
       
