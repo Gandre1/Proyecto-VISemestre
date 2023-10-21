45cfbd7176e373d2f198fcb8f06190cd1570141c
Gandre1 <116277140+Gandre1@users.noreply.github.com>
2023-10-20 19:18:04 -0500

    Update PaginaDesarrollo.js

--- src/pages/PaginaDesarrollo.js
+++ src/pages/PaginaDesarrollo.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import FormularioDesarrollo from '../components/FormularioDesarrollo';
-import Navbar from '../components/Navbar';
 
 function PaginaDesarrollo() {
   return (
     <div>
-      <Navbar />
       <center><h1>Desarrollo</h1></center>  
       <FormularioDesarrollo />
     </div>
