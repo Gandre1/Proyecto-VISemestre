19937fd52a26a418aa346c35cc7dd554c9c61cc3
Gandre1 <116277140+Gandre1@users.noreply.github.com>
2023-10-20 19:17:07 -0500

    Update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 import PaginaFinalizacion from '../pages/PaginaFinalizacion'
 import PaginaInicio from '../pages/PaginaInicio'
 import PaginaDesarrollo from '../pages/PaginaDesarrollo'
-
+import PaginaPrincipal from '../pages/PaginaPrincipal'
 
 const routes = [
-  { path: '/', exact: true, name: 'Home'},
+  { path: '/', name: 'PaginaIncio', element: PaginaPrincipal},
   { path: '/Inicio', name: 'PaginaIncio', element: PaginaInicio },
   { path: '/Desarrollo', name: 'PaginaDesarrollo', element: PaginaDesarrollo},
   { path: '/Finalizacion', name: 'PaginaFinalizacion', element: PaginaFinalizacion }
